37d10140762a4dde1c282ee176fcc8637f64f9ca
wyuss <inelli447@gmail.com>
2021-12-17 15:58:52 +0300

    Task 21 finishd

---
+++ src/main/java/com/blockwit/learn1/wyuss/Task21.java
@@ -0,0 +1,13 @@
+package com.blockwit.learn1.wyuss;
+
+public class Task21 {
+
+    public static void main(String[] arg) {
+        int[] num = {23, 56, 21, 67, 98};
+
+        for (int i = num.length - 1  ; i >= 0; i--) {
+            System.out.println(num[i]);
+
+        }
+    }
+}
