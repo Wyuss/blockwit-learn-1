d300ee8d9976e6357b0b4e77ec1ac9baacd281b4
wyuss <inelli447@gmail.com>
2022-01-30 18:26:07 +0300

    Добавила Тu0

--- src/main/java/com/blockwit/learn1/wyuss/Tu0.java
+++ src/main/java/com/blockwit/learn1/wyuss/Tu0.java
@@ -1,19 +1,33 @@
 package com.blockwit.learn1.wyuss;
-import java.util.Arrays;
 public class Tu0 {
     public static void main(String[] arg) {
         /* Задан массив чисел 12,7,89,-4,8,9. Необходимо:
          1. Распечатать его.
          2. Отсортировать его.
          3. Распечатать еще раз.*/
-        int [] num = {12, 7, 89, -4, 8, 9};
+        int [] array = {12, 7, 89, -4, 8, 9};
         System.out.print("Исходные числа: ");
-        for (int i = 0; i < num.length - 1; i++) {
-            System.out.print(num[i] + ", ");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
         }
-            System.out.println( num[num.length - 1] + ".");
+            System.out.println();
+            int temp;
+            boolean sorted = false;
+            while (!sorted) {
+                sorted = true;
 
-        Arrays.sort(num);
-        System.out.println( "Числа в порядке возрастания: " + Arrays.toString(num) + ".");
+                for (int i = 0; i<array.length-1; i++){
+                    if (array[i] > array[i+1]){
+                        temp = array[i];
+                        array[i] = array[i+1];
+                        array[i+1]= temp;
+                        sorted = false;
+                    }
+                }
+            }
+            System.out.print("Числа в порядке возрастания: ");
+            for (int i : array) {
+                System.out.print( i + " ");
+            }
     }
 }
