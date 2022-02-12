eca421216fd329905b18f08a1428fbb633cce2f0
wyuss <inelli447@gmail.com>
2022-02-12 19:35:30 +0300

    Task S7T4 is done.

---
+++ src/main/java/com/blockwit/learn1/wyuss/ProtoList11.java
@@ -0,0 +1,18 @@
+package com.blockwit.learn1.wyuss;
+
+public class ProtoList11 {
+
+    int[] array ;
+
+    public void create( int size) {
+        array = new int [size];
+    }
+
+    public int getSize(int size) {
+        return size;
+    }
+
+    public int get(int i) {
+        return array[i];
+    }
+}
