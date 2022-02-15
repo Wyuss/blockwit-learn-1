ff423bef56c74928dc095be0c61e84ae93b4630f
wyuss <inelli447@gmail.com>
2022-02-15 16:16:05 +0300

    Task S8T1 into MyIntArrayList is done.

---
+++ src/main/java/com/blockwit/learn1/wyuss/MyIntArrayList.java
@@ -0,0 +1,41 @@
+package com.blockwit.learn1.wyuss;
+
+public class MyIntArrayList {
+
+    int[] list;
+    int count;
+
+    public MyIntArrayList() {
+        list = new int[0];
+        count = 0;
+    }
+
+    public int add (int num) {
+
+        if (count == list.length) {
+            int[] newList = new int [list.length +1];
+            for (int i = 0; i < list.length; i++) {
+                newList[i]= list[i];
+            }
+            newList[newList.length - 1] = num;
+            list = newList;
+        } else {
+            list[count - 1] = num;
+        }
+        count++;
+        return list.length  - 1;
+    }
+
+    public int get (int i) {
+        return list[i];
+    }
+
+    public int size () {
+        return list.length;
+    }
+    public void clear () {
+        for (int i = 0; i < list.length; i++) {
+            list [i] = 0;
+        }
+    }
+}
